3d3d56b95bee743c006c8e2c8f61623ffae26398
neethees <136804136+neethees@users.noreply.github.com>
2023-06-16 11:52:54 -0400

    Add files via upload

---
+++ program.py
@@ -0,0 +1,11 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, may 15 2023, 21:22:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=12
+>>> print(age)
+13
+>>> Weight=34
+>>> print(Weight)
+60
+>>> Height=150
+>>> print(Height)
+150
